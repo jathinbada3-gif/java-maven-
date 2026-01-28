90b096b32c5dab9c44b8055b2c6244ffa323a542
jathinbada3-gif <jathinbada3@gmail.com>
2026-01-28 13:33:50 +0530

    multplication removed

--- src/main/java/devops/calc/Mydevops.java
+++ src/main/java/devops/calc/Mydevops.java
@@ -18,7 +18,7 @@ public class Mydevops {
 
 	        System.out.println("Addition: " + (20 + 30));
 	        System.out.println("Subtraction: " + (50 - 20));
-	        System.out.println("Multiplication: " + (60 * 5));
+	      
 
 	        if (b != 0) {
 	            System.out.println("Division: " + (40 / 2));
