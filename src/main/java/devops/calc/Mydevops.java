7e8eb463902c51a1538676672cd45826862dc643
jathinbada3-gif <jathinbada3@gmail.com>
2026-01-28 13:09:51 +0530

    calc project

---
+++ src/main/java/devops/calc/Mydevops.java
@@ -0,0 +1,31 @@
+package devops.calc;
+
+import java.util.Scanner;
+
+public class Mydevops {
+	
+
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Enter first number:");
+	        int a = sc.nextInt();
+
+	        System.out.println("Enter second number:");
+	        int b = sc.nextInt();
+
+	        System.out.println("Addition: " + (20 + 30));
+	        System.out.println("Subtraction: " + (50 - 20));
+	        System.out.println("Multiplication: " + (60 * 5));
+
+	        if (b != 0) {
+	            System.out.println("Division: " + (40 / 2));
+	        } else {
+	            System.out.println("Division: Not possible (divide by zero)");
+	        }
+
+	        sc.close();
+	    }
+	}
