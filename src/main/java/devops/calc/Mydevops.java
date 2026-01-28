465806719161377a78447c0c4703e530d5e9f2a5
jathinbada3-gif <jathinbada3@gmail.com>
2026-01-28 15:10:59 +0530

    added 6

--- src/main/java/devops/calc/Mydevops.java
+++ src/main/java/devops/calc/Mydevops.java
@@ -1,31 +1,44 @@
 package devops.calc;
 
-import java.util.Scanner;
-
 public class Mydevops {
-	
 
+    // Calculator methods
+    public int sum(int a, int b) {
+        return a + b;
+    }
 
-	    public static void main(String[] args) {
+    public int diff(int a, int b) {
+        return a - b;
+    }
 
-	        Scanner sc = new Scanner(System.in);
+    public int mul(int a, int b) {
+        return a * b;
+    }
 
-	        System.out.println("Enter first number:");
-	        int a = sc.nextInt();
+    public int div(int a, int b) {
+        return a / b;
+    }
 
-	        System.out.println("Enter second number:");
-	        int b = sc.nextInt();
+    public int mod(int a, int b) {
+        return a % b;
+    }
 
-	        System.out.println("Addition: " + (a + b));
-	        System.out.println("Subtraction: " + (a - b));
-	        
+    public void show() {
+        System.out.println("Hello GitHub Actions");
+    }
 
-	        if (b != 0) {
-	            System.out.println("Division: " + (a / b));
-	        } else {
-	            System.out.println("Division: Not possible (divide by zero)");
-	        }
+    public static void main(String[] args) {
 
-	        sc.close();
-	    }
-	}
+        // Create object
+        Mydevops calc = new Mydevops();
+
+        // Use the same object
+        System.out.println("Sum is " + calc.sum(20, 10));
+        System.out.println("Diff is " + calc.diff(20, 10));
+        System.out.println("Mul is " + calc.mul(20, 10));
+        System.out.println("Div is " + calc.div(20, 10));
+        System.out.println("Mod is " + calc.mod(20, 10));
+
+        calc.show();
+    }
+}
